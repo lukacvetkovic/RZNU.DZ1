2e29f0b8710fefba8e091ae32af16cce8f053255
Luka Cvetkovic <cvetko911@hotmail.com>
2016-11-06 16:48:38 +0100

    Izrada Log-a podataka

--- RZNU.DZ1/App_Start/WebApiConfig.cs
+++ RZNU.DZ1/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using RZNU.DZ1.ActionFilters;
 
 namespace RZNU.DZ1
 {
@@ -10,6 +11,7 @@ namespace RZNU.DZ1
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Filters.Add(new LoggingFilterAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
